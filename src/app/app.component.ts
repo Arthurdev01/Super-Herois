d7bc812c7b64f24253bf75ef524c9de07eef482f
arthur <arthur.spinosa12@gmail.com>
2026-04-14 16:54:41 -0300

    ✨ feat: exibe resposta da API no console

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { Data, RouterOutlet } from '@angular/router';
+import { SuperHeroService } from '../service/super-hero.service';
+
 
 @Component({
   selector: 'app-root',
@@ -7,6 +9,18 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'super_herois';
+  herois: any; //propriedade pertence a uma classe
+
+    constructor(private superhero:SuperHeroService) {
+  }
+
+  ngOnInit(): void {
+    this.superhero.getHerois().subscribe({ //Observable = algo que entrega dados ao longo do tempo. o Subscribe Quero escutar quando esse dado chegar
+  next: (data) => this.herois = data, // aqui basicamente estou pegando o valor que vem da minha api, e salvando ela na minha propriedade herois
+  error: (err) => console.error('Error occurred:', err)
+})
+
+  }
 }
