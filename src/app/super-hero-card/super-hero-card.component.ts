32f4bcbf8c5f97fc3c4e0c854640dac9d0fdd8a5
arthur <arthur.spinosa12@gmail.com>
2026-04-14 21:32:46 -0300

    ✨ feat(heroes): adiciona componente super-hero-card

---
+++ src/app/super-hero-card/super-hero-card.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-super-hero-card',
+  imports: [],
+  templateUrl: './super-hero-card.component.html',
+  styleUrl: './super-hero-card.component.css'
+})
+export class SuperHeroCardComponent {
+
+}
