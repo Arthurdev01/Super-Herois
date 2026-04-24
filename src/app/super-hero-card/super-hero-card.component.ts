09cb98e7b707f49cee378f3b51adb06515ed9447
arthur <arthur.spinosa12@gmail.com>
2026-04-23 23:47:15 -0300

    ✨ feat(super-hero-card): reorganiza o layout e a exibição dos detalhes do herói

--- src/app/super-hero-card/super-hero-card.component.ts
+++ src/app/super-hero-card/super-hero-card.component.ts
@@ -1,12 +1,112 @@
 import { Component, Input } from '@angular/core';
-import { SuperHeroService } from '../../service/super-hero.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-super-hero-card',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './super-hero-card.component.html',
   styleUrl: './super-hero-card.component.css'
 })
 export class SuperHeroCardComponent {
-@Input() heroi: any = null // aqui ele basicamente recebe o heroi de fora 
+  @Input() heroi: any = null;
+
+  get powerStats() {
+    return [
+      {
+        name: 'Inteligência', value: Number(this.heroi?.powerstats?.intelligence) || 0
+      },
+      {
+        name: 'Força', value: Number(this.heroi?.powerstats?.strength) || 0
+      },
+      {
+        name: 'Velocidade', value: Number(this.heroi?.powerstats?.speed) || 0
+      },
+      {
+        name: 'Durabilidade', value: Number(this.heroi?.powerstats?.durability) || 0
+      },
+      {
+        name: 'Poder', value: Number(this.heroi?.powerstats?.power) || 0
+      },
+      {
+        name: 'Combate', value: Number(this.heroi?.powerstats?.combat) || 0
+      }
+    ];
+  }
+get appearance() {
+  return [
+    {
+      name: 'Gênero',
+      value: this.heroi?.appearance?.gender || 'Não informado'
+    },
+    {
+      name: 'Raça',
+      value: this.heroi?.appearance?.race || 'Não informado'
+    },
+    {
+      name: 'Altura',
+      value: this.heroi?.appearance?.height?.[1] || 'Não informado'
+    },
+    {
+      name: 'Peso',
+      value: this.heroi?.appearance?.weight?.[1] || 'Não informado'
+    },
+    {
+      name: 'Olhos',
+      value: this.heroi?.appearance?.eyeColor || 'Não informado'
+    },
+    {
+      name: 'Cabelo',
+      value: this.heroi?.appearance?.hairColor || 'Não informado'
+    }
+  ];
 }
+get biografia() {
+  return [
+    {
+      name: 'Nome completo', value: this.heroi?.biography?.fullName || 'Não informado'
+    },
+    {
+      name: 'Alter ego', value: this.heroi?.biography?.alterEgos || 'Não informado'
+    },
+    {
+      name: 'Apelidos', value: this.heroi?.biography?.aliases || 'Não informado'
+    },
+    {
+      name: 'Nascimento', value: this.heroi?.biography?.placeOfBirth || 'Não informado'
+    },
+    {
+      name: 'Primeira aparição', value: this.heroi?.biography?.firstAppearance || 'Não informado'
+    },
+    {
+      name: 'Editora', value: this.heroi?.biography?.publisher || 'Não informado'
+    },
+    {
+    name: 'Alinhamento', value: this.heroi?.biography?.alignment || 'Não informado'
+  }
+  ];
+}
+get trabalho() {
+  return [
+    {
+      name: 'Ocupação', value: this.heroi?.work?.occupation || 'Não informado'
+    },
+    {
+      name: 'Base', value: this.heroi?.work?.base || 'Não informado'
+    }
+  ];
+}
+get conexoes() {
+  return [
+    {
+      name: 'Grupo', value: this.heroi?.connections?.groupAffiliation || 'Não informado'
+    },
+    {
+      name: 'Parentes', value: this.heroi?.connections?.relatives || 'Não informado'
+    }
+  ];
+}
+}
+
+
+
+
