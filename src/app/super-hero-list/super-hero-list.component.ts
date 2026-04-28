a849538047d3f484186169f5491d459ee166f70e
arthur <arthur.spinosa12@gmail.com>
2026-04-28 04:35:38 -0300

    ✨ feat: adiciona busca e filtro por publisher na lista de heróis

--- src/app/super-hero-list/super-hero-list.component.ts
+++ src/app/super-hero-list/super-hero-list.component.ts
@@ -1,25 +1,42 @@
 import { CommonModule } from '@angular/common';
 import { Component, OnInit, AfterViewInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { SuperHeroService } from '../../service/super-hero.service';
 import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-super-hero-list',
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule],
   templateUrl: './super-hero-list.component.html',
   styleUrl: './super-hero-list.component.css'
 })
 export class SuperHeroListComponent implements OnInit, AfterViewInit {
-  herois: any;
+  herois: any[] = [];
+  busca: string = '';
+  filtroPublisher: string = 'todos';
 
-  constructor(private superhero: SuperHeroService, private router: Router) {}
+  constructor(
+    private superhero: SuperHeroService,
+    private router: Router
+  ) {}
 
-  ngOnInit(): void {
-    this.superhero.getHerois().subscribe({
-      next: (data: any) => this.herois = data,
-      error: (err) => console.error('Error occurred:', err)
-    });
+ngOnInit(): void {
+  const buscaSalva = sessionStorage.getItem('buscaHerois');
+  const publisherSalvo = sessionStorage.getItem('filtroPublisher');
+
+  if (buscaSalva) {
+    this.busca = buscaSalva;
   }
+
+  if (publisherSalvo) {
+    this.filtroPublisher = publisherSalvo;
+  }
+
+  this.superhero.getHerois().subscribe({
+    next: (data: any) => this.herois = data,
+    error: (err) => console.error('Error occurred:', err)
+  });
+}
 
   ngAfterViewInit(): void {
     const scrollSalvo = sessionStorage.getItem('scrollListaHerois');
@@ -34,10 +51,30 @@ export class SuperHeroListComponent implements OnInit, AfterViewInit {
     }
   }
 
-  irParaDetails(heroi: any) {
-    sessionStorage.setItem('scrollListaHerois', String(window.scrollY));
-    this.router.navigate(['heroi', heroi.id]);
-  }
+heroisFiltrados() {
+  return this.herois.filter((heroi: any) => {
+    const nomeHeroi = heroi?.name?.toLowerCase() || '';
+    const buscaDigitada = this.busca.toLowerCase();
+
+    const passouNaBusca = nomeHeroi.includes(buscaDigitada);
+
+    const publisherHeroi = heroi?.biography?.publisher || '';
+
+    const passouNoPublisher =
+      this.filtroPublisher === 'todos' ||
+      publisherHeroi === this.filtroPublisher;
+
+    return passouNaBusca && passouNoPublisher;
+  });
+}
+
+irParaDetails(heroi: any) {
+  sessionStorage.setItem('scrollListaHerois', String(window.scrollY));
+  sessionStorage.setItem('buscaHerois', this.busca);
+  sessionStorage.setItem('filtroPublisher', this.filtroPublisher);
+
+  this.router.navigate(['heroi', heroi.id]);
+}
 
   getPowerStats(heroi: any) {
     return [
